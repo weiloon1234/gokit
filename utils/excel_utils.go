ea835c2794e7ab691be87e71cf748c477dd803ca
ws <weiloon1234@gmail.com>
2024-12-03 20:16:47 +0800

    init

--- utils/excel_utils.go
+++ utils/excel_utils.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"github.com/xuri/excelize/v2"
 )
 
@@ -26,9 +27,37 @@ func GenerateExcelFile(fileName string, headerColumns []string, data [][]interfa
 	for rowIndex, row := range data {
 		for colIndex, value := range row {
 			cell, _ := excelize.CoordinatesToCellName(colIndex+1, rowIndex+2)
-			err := f.SetCellValue(sheetName, cell, value)
-			if err != nil {
-				return nil, err
+			switch v := value.(type) {
+			case string:
+				// Set as string value
+				err := f.SetCellValue(sheetName, cell, v)
+				if err != nil {
+					return nil, err
+				}
+			case float64, float32, int, int64:
+				// Convert numbers to string to prevent scientific notation issues
+				err := f.SetCellValue(sheetName, cell, fmt.Sprintf("'%v", v)) // Prepend apostrophe to treat as text
+				if err != nil {
+					return nil, err
+				}
+			case bool:
+				// Set boolean values directly
+				err := f.SetCellValue(sheetName, cell, v)
+				if err != nil {
+					return nil, err
+				}
+			case nil:
+				// Handle nil values (set cell as empty)
+				err := f.SetCellValue(sheetName, cell, "")
+				if err != nil {
+					return nil, err
+				}
+			default:
+				// Default case to treat value as string
+				err := f.SetCellValue(sheetName, cell, fmt.Sprintf("'%v", v)) // Prepend apostrophe to treat as text
+				if err != nil {
+					return nil, err
+				}
 			}
 		}
 	}
