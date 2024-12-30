81e723ea56f96e63aaea2b74ca9d5c4173e2b752
weiloonso <weiloon1234@gmail.com>
2024-12-31 01:45:42 +0800

    Enhance IsAdminMiddleware to set admin context based on request path

--- middleware/is_admin_middleware.go
+++ middleware/is_admin_middleware.go
@@ -13,12 +13,18 @@ const IsAdminKey contextKey = "is_admin"
 
 func IsAdminMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var isAdmin bool
 		if strings.HasPrefix(c.Request.URL.Path, "/api/admin") {
-			ctx := context.WithValue(c.Request.Context(), IsAdminKey, true)
-
-			c.Request = c.Request.WithContext(ctx)
-			c.Next()
+			isAdmin = true
+		} else {
+			isAdmin = false
 		}
+
+		ctx := context.WithValue(c.Request.Context(), IsAdminKey, isAdmin)
+		c.Set(string(IsAdminKey), isAdmin)
+
+		c.Request = c.Request.WithContext(ctx)
+		c.Next()
 	}
 }
 
